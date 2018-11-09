3a794de77ba03a25afe5fc415a6acd3bc7e9777f
daandradec <daandradec@unal.edu.co>
2018-11-08 19:20:24 -0500

    image trait bug

--- app/Http/Controllers/Apis/User/Traits/ImageUserTrait.php
+++ app/Http/Controllers/Apis/User/Traits/ImageUserTrait.php
@@ -10,7 +10,9 @@ use App\User;
 trait ImageUserTrait{
 
     public function image($id){    
-        return response()->json(["a"=>User::findOrFail($id)->avatar,"b"=>Storage::url(User::findOrFail($id)->avatar),"c"=>storage_path()]);
+        //return response()->json(["a"=>User::findOrFail($id)->avatar,"b"=>Storage::url(User::findOrFail($id)->avatar),"c"=>storage_path()]);
+        dd(Storage::get(User::findOrFail($id)->avatar));
+        return "hola";
         //return Image::make(Storage::get(User::findOrFail($id)->avatar))->response();        
     }
 
