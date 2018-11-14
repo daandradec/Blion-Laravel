bf44b8b225190a41f60e959a4d18fe11fa7f72f9
daandradec <daandradec@unal.edu.co>
2018-11-14 16:23:50 -0500

    com

--- app/Http/Controllers/Apis/User/Traits/ImageUserTrait.php
+++ app/Http/Controllers/Apis/User/Traits/ImageUserTrait.php
@@ -23,9 +23,9 @@ trait ImageUserTrait{
         $user = User::findOrFail($id);
 
 
-        Storage::put('/public/foo.png',base64_decode($str_img));
+        Storage::put('public/foo.png',base64_decode($str_img));
         
-        $path = Storage::putFile('public/Users/'.$user->email,new File( public_path(Storage::url('public/foo.png')) ));
+        $path = Storage::putFile('/public/Users/'.$user->email,new File( public_path(Storage::url('public/foo.png')) ));
 
         Storage::delete('public/foo.png');
 
