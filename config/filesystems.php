1b3270b8987e69654c3b37ec159f33616fdc4ddc
daandradec <daandradec@unal.edu.co>
2018-11-14 19:42:14 -0500

    commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,13 +47,13 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
-        /*
+        
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-        ],*/
+        ],
 
         's3' => [
             'driver' => 's3',
@@ -63,12 +63,13 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
+        /*
         'public' => [
             'driver' => 'local',
             'root' => public_path(),
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
-        ],
+        ],*/
     ],
 
 ];
