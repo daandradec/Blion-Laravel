d577953e27374f8776ff31d861ccff653baf4759
daandradec <daandradec@unal.edu.co>
2019-02-10 15:38:15 -0500

    csrf_token

--- app/Http/Controllers/Apis/Auth/Traits/LoginValidatorTrait.php
+++ app/Http/Controllers/Apis/Auth/Traits/LoginValidatorTrait.php
@@ -47,7 +47,7 @@ trait LoginValidatorTrait{
             $token->update(['csrf'=>csrf_token(),'expired'=>Carbon::now()->addDays(1)]);
         
         $array["auth_token"] = $token->csrf;
-        $array["expired_date_token"] = $token->expired->format('Y m d H i s');
+        $array["expired_date_token"] = $token->expired->format('Y-m-d');
         return $array;
     }
 
